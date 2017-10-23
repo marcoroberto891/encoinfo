cf95545d0d56dc2b3795ffda6552b69a157f38f2
marcoroberto891 <marcorjfaria@gmail.com>
2017-10-23 20:38:00 -0300

    commit
    commit initial

---
+++ inscricoes/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    login = models.CharField('login',max_length=100)
+    senha = models.CharField('senha',max_length=6)
+
+
+class Pessoa(models.Model):
+    nome = models.CharField('nome',max_length=100)
+    cpf = models.CharField('cpf',max_length=11)
+    is_admin = models.ForeignKey(User, related_name='is_admin', null=True,blank=False)
+
+class Evento(models.Model):
+    nome = models.CharField('nome',max_length=50)
+    descricao = models.CharField('cpf',max_length=100)
+    dataInicio = models.DateField()
+    dataFim = models.DateField()
+
+class Inscricao(models.Model):
+    nome = models.CharField('nome',max_length=100)
+    cpf = models.CharField('cpf',max_length=11)
+    is_admin = models.ForeignKey(User, related_name='is_admin', null=True,blank=False)
+
